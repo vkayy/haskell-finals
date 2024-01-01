6d2d89528f0f8e6803c77f68c52f38c3fdf140c0
vkayy <vkrice2475@gmail.com>
2024-01-01 02:56:50 +0000

    feat: add parts 1-3 for 2019

--- 2019. SAT Solving/Main.hs
+++ 2019. SAT Solving/Main.hs
@@ -26,17 +26,23 @@ printF
 lookUp :: Eq a => a -> [(a, b)] -> b
 -- Pre: The item being looked up has a unique binding in the list
 lookUp 
-  = undefined
+  = (fromJust .) . lookup
 
 -- 3 marks
 vars :: Formula -> [Id]
-vars 
-  = undefined
+vars (Var id)
+  = [id]
+vars (Not f)
+  = sort . nub $ vars f
+vars (And f f')
+  = sort . nub $ vars f ++ vars f'
+vars (Or f f')
+  = sort . nub $ vars f ++ vars f'
 
 -- 1 mark
 idMap :: Formula -> IdMap
-idMap 
-  = undefined
+idMap
+  = (`zip` [1..]) . vars
 
 --------------------------------------------------------------------------
 -- Part II
@@ -54,37 +60,76 @@ distribute a b
 
 -- 4 marks
 toNNF :: Formula -> NNF
-toNNF 
-  = undefined
+toNNF (And f f')
+  = And (toNNF f) (toNNF f')
+toNNF (Or f f')
+  = Or (toNNF f) (toNNF f')
+toNNF (Not (Or f f'))
+  = And (toNNF $ Not f) (toNNF $ Not f')
+toNNF (Not (And f f'))
+  = Or (toNNF $ Not f) (toNNF $ Not f')
+toNNF (Not (Not f))
+  = toNNF f
+toNNF f
+  = f
+
 
 -- 3 marks
 toCNF :: Formula -> CNF
-toCNF 
-  = undefined
+toCNF
+  = toCNF' . toNNF
+  where
+    toCNF' :: NNF -> CNF
+    toCNF' (And f f')
+      = And (toCNF f) (toCNF f')
+    toCNF' (Or f f')
+      = distribute (toCNF f) (toCNF f')
+    toCNF' f
+      = f
 
 -- 4 marks
 flatten :: CNF -> CNFRep
-flatten 
-  = undefined
+flatten f
+  = flatten' (idMap f) f
+  where
+    flatten' :: IdMap -> CNF -> CNFRep
+    flatten' idm (Or f f')
+      = [concat $ flatten' idm f ++ flatten' idm f']
+    flatten' idm (And f f')
+      = flatten' idm f ++ flatten' idm f'
+    flatten' idm (Not (Var id))
+      = [[negate $ lookUp id idm]]
+    flatten' idm (Var id)
+      = [[lookUp id idm]]
 
 --------------------------------------------------------------------------
 -- Part III
 
 -- 5 marks
 propUnits :: CNFRep -> (CNFRep, [Int])
-propUnits 
-  = undefined
+propUnits cnf
+  | null ucs  = (cnf, [])
+  | otherwise = (cnf', uc : ucs')
+  where
+    ucs          = [l | l:ls <- cnf, null ls]
+    (uc: _)      = ucs
+    cld          = filter (notElem uc) cnf
+    ltd          = map (filter (/= -uc)) cld
+    (cnf', ucs') = propUnits ltd
 
--- 4 marks
 dp :: CNFRep -> [[Int]]
-dp 
-  = undefined
+dp cnf
+  | null cnf'     = [ucs']
+  | any null cnf' = []
+  | otherwise     = concatMap (map (ucs' ++)) [trem, frem]
+  where
+    (cnf', ucs')   = propUnits cnf
+    ((uc : _) : _) = cnf'
+    (trem, frem)   = (dp ([uc] : cnf'), dp ([-uc] : cnf'))
 
 --------------------------------------------------------------------------
 -- Part IV
 
 -- Bonus 2 marks
 allSat :: Formula -> [[(Id, Bool)]]
-allSat
   = undefined
-
